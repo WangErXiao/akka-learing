cfccb929da1708ec691351e24ac002d06cdf0a4d
robin <863579016@qq.com>
2015-02-05 09:36:50 +0800

    TypeActor example

--- src/main/java/com/akka/typeactor/AppContext.java
+++ src/main/java/com/akka/typeactor/AppContext.java
@@ -3,7 +3,10 @@ package com.akka.typeactor;
 import akka.actor.ActorSystem;
 import akka.actor.TypedActor;
 import akka.actor.TypedProps;
+import akka.util.Timeout;
+import scala.concurrent.Await;
 import scala.concurrent.Future;
+import scala.concurrent.duration.FiniteDuration;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,10 +19,11 @@ public class AppContext {
         CalculatorInt calculator= TypedActor.get(system)
                 .typedActorOf(TypedProps.apply(CalculatorInt.class,Calculator.class));
         Future<Integer> result=calculator.add(12, 2);
+        Timeout timeout=new Timeout(FiniteDuration.create(5, TimeUnit.SECONDS));
         //calculator.subtract(3,1);
         //calculator.incrementCount();
         System.out.println("start-----------");
-        while (true){
+        /*while (true){
             if(result.isCompleted()){
                 System.out.println(result.value().toString());
                 break;
@@ -30,6 +34,11 @@ public class AppContext {
                     e.printStackTrace();
                 }
             }
+        }*/
+        try {
+            Await.result(result,timeout.duration());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         System.out.println("finish---------");
         system.shutdown();
